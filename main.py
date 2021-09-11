5d287f71f3f6a29a9d84f09811d317ce49e11d51
wizbeans <75798911+wizbeans@users.noreply.github.com>
2021-09-11 16:04:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import responses
 
 API_KEY = 'donot_copy_my_key'
 
-
-
-# Set up the logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logging.info('Starting Bot...')
 
@@ -27,17 +24,13 @@ def handle_message(update, context):
     text = str(update.message.text).lower()
     logging.info(f'User ({update.message.chat.id}) says: {text}')
 
-    # Bot response
     response = responses.get_response(text)
     update.message.reply_text(response)
 
 
 def error(update, context):
-    # Logs errors
     logging.error(f'Update {update} caused error {context.error}')
 
-
-# Run the programme
 if __name__ == '__main__':
     updater = Updater(API_KEY, use_context=True)
     dp = updater.dispatcher
@@ -46,13 +39,8 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler('start', start_command))
     dp.add_handler(CommandHandler('help', help_command))
     dp.add_handler(CommandHandler('custom', custom_command))
-
-    # Messages
     dp.add_handler(MessageHandler(Filters.text, handle_message))
 
-    # Log all errors
     dp.add_error_handler(error)
-
-    # Run the bot
     updater.start_polling(1.0)
     updater.idle()
